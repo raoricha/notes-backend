9fbc9cccca0716ca43c319fb7c07344db66da8f2
Richa Rao <raoricha115@gmail.com>
2023-02-21 01:14:17 +0530

    added build

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors())
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -9,13 +9,14 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
-app.use(express.static('dist'))
+
 const unknownEndPoint = (request, response) => {
     response.status(404).send({ error: 'unknwon endpoint' })
 }
-
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -85,7 +86,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndPoint)
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
